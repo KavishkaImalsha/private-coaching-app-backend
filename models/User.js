09932a5580696ee60153690b70f34939879618ba
Kavishka Imalsha <kavishkaimalsha@gmail.com>
2026-04-07 15:16:57 +0530

    Create user schema

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+    },
+    googleId: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.Schema('User', userSchema)
